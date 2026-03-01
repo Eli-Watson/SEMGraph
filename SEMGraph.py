a1ddf39d39ae6b1dbb94bd2ec1c9ee1df75d64e7
Drvulcanlord <122398995+Eli-Watson@users.noreply.github.com>
2026-02-28 21:01:41 -0600

    Update SEMGraph.py

--- SEMGraph.py
+++ SEMGraph.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #  ____  _____ __  __  ____                 _
 # / ___|| ____|  \/  |/ ___|_ __ __ _ _ __ | |__
 # \___ \|  _| | |\/| | |  _| '__/ _` | '_ \| '_ \
@@ -14,19 +15,25 @@
 
 import cmd
 import os
+
+import inquirer
 import pandas as pd
-import plotly.io as pio
 import plotly.graph_objects as go
+import plotly.io as pio
 import pyfiglet
 from colorama import Fore, init
-import inquirer
 
 # option for colorama to reset the color after each use, otherwise it changes for the entire program
 init(autoreset=True)
 
 class CLI(cmd.Cmd):
-    prompt = '>> '
-    welcome = 'Welcome to the Shell Eco Marathon Graphing (SEMGraph) tool. By Eli Watson, Bearcat Motorsports'
+    #These are the graphs avalibe to be used, by both graph_and graph select.
+    ENGINE_AGNOSTIC_GRAPHS = ["Map","Speed-Dist",]
+    ICE_GRAPHS = ["Map-Flow","Flow-Dist","Acel-Speed-Dotplot","CorrFlow-Dist",]
+    BE_GRAPHS = ["Joule-Dist","Joule-map","Current-Dist",]
+    
+    prompt = ">> "
+    welcome = "Welcome to the Shell Eco Marathon Graphing (SEMGraph) tool. By Eli Watson, Bearcat Motorsports"
 
     # Helper Functions
     def print_banner(self):
@@ -35,130 +42,157 @@ class CLI(cmd.Cmd):
 
     def preloop(self):
         # This method is automatically called before the CmdLoop starts.
-        self.print_banner()  
+        self.print_banner()
         print(self.welcome)
         print('Type "Help" for a list of available options')
 
     def select_data_file(self):
-        """Helper method to handle file selection process using inquirer."""
-        print('What Data Directory would you like to list? Select from the list below with your arrow keys. Hit Enter over the option you want.')
+        base_data_dir = "./Data/"
 
+        if not os.path.exists(base_data_dir):
+            print(Fore.YELLOW + f"Base data directory '{base_data_dir}' does not exist.")
+            return None, None
+    
+        # Get only folders inside ./Data/
+        data_dirs = [
+            d for d in os.listdir(base_data_dir)
+            if os.path.isdir(os.path.join(base_data_dir, d))
+        ]
+    
+        if not data_dirs:
+            print(Fore.YELLOW + "No data directories found inside ./Data/")
+            return None, None
+
+        # Prompt user to select a directory
         questions = [
             inquirer.List(
                 "Dir",
-                message=Fore.RED + "What Data dir would you like to list?",
-                choices=["Urban", "Proto", "Sample"],
+                message=Fore.RED + "Select a data directory:",
+                choices=data_dirs,
             ),
         ]
-        question_dir = inquirer.prompt(questions)["Dir"]
-        selected_dir = question_dir
-        dir_to_list = ''
+    
+        selected_dir = inquirer.prompt(questions)["Dir"]
+        dir_to_list = os.path.join(base_data_dir, selected_dir)
 
-        match selected_dir:
-            case "Urban":
-                dir_to_list = "./Data/Urban/"
-            case "Proto":
-                dir_to_list = "./Data/Proto/"
-            case "Sample":
-                dir_to_list = "./Data/Sample/"
-
-        if os.path.exists(dir_to_list):
-            print(Fore.RED + f"Listing files in {dir_to_list}:")
-            files = self.list_files_recursive(dir_to_list)
-        else:
-            print(Fore.YELLOW + f"Directory {dir_to_list} does not exist.")
-            return None, None
-
-        # inquirer prompt to select a file to graph
-        if files:
-            questions_files = [
-                inquirer.List(
-                    "graph_file",
-                    message="Which file would you like to graph?",
-                    choices=files,
-                ),
-            ]
-            graphing_source = inquirer.prompt(questions_files)["graph_file"]
-            print(graphing_source)
-            file_path = os.path.join(dir_to_list, graphing_source)
-
-            if not os.path.exists(file_path):
-                print(Fore.YELLOW + f"Error: The file '{graphing_source}' does not exist in the directory '{dir_to_list}'")
-                return None, None
-
-            return file_path, dir_to_list
-        else:
+        print(Fore.RED + f"Listing files in {dir_to_list}:")
+    
+        files = self.list_files_recursive(dir_to_list)
+    
+        if not files:
             print(Fore.YELLOW + f"No files found in {dir_to_list}.")
             return None, None
+    
+        # Prompt user to select a file
+        questions_files = [
+            inquirer.List(
+                "graph_file",
+                message="Which file would you like to graph?",
+                choices=files,
+            ),
+        ]
+    
+        graphing_source = inquirer.prompt(questions_files)["graph_file"]
+        file_path = os.path.join(dir_to_list, graphing_source)
+
+        if not os.path.exists(file_path):
+            print(
+                Fore.YELLOW
+                + f"Error: The file '{graphing_source}' does not exist in '{dir_to_list}'"
+            )
+            return None, None
+
+        return file_path, dir_to_list
 
     def list_files_recursive(self, dir_path):
         """Recursively lists all files in a directory and its subdirectories."""
         all_files = []
-        
+
         for root, dirs, files in os.walk(dir_path):
             for file in files:
                 # Append the full relative path of each file found
                 relative_path = os.path.relpath(os.path.join(root, file), dir_path)
                 all_files.append(relative_path)
-        
+
         return all_files
 
     # These are the actual graphs being generated
     def generate_graph(self, df, graph_type):
         """Helper method to generate graphs based on user selection."""
         match graph_type:
-            case 'Map':
-                print('Generating map...')
-                fig = df.plot(x='gps_longitude', y='gps_latitude', color='lap_lap')
-            
-            case 'Speed-Dist':
-                print('Generating speed-dist...')
-                fig = df.plot(x='lap_dist', y='gps_speed', color='lap_lap')
-            
-            case 'Map-Flow':
-                print('Generating map-flow...')
-                fig = df.plot(x='gps_longitude', y='gps_latitude', color='lfm_instantflow', kind='scatter', facet_col='lap_lap', facet_col_wrap=4)
-            
-            case 'Flow-Dist':
-                print('Generating flow-dist...')
-                fig = df.plot(x='lap_dist', y='lfm_instantflow', color='lap_lap')
-            
-            case 'Acel-Speed-Dotplot':
-                print('Generating acel-speed-dotplot...')
-                df['acceleration'] = 0.
-                df['consumption'] = 0.
+            case "Map":
+                print("Generating map...")
+                fig = df.plot(x="gps_longitude", y="gps_latitude", color="lap_lap")
+
+            case "Speed-Dist":
+                print("Generating speed-dist...")
+                fig = df.plot(x="lap_dist", y="gps_speed", color="lap_lap")
+
+            case "Map-Flow":
+                print("Generating map-flow...")
+                fig = df.plot(
+                    x="gps_longitude",
+                    y="gps_latitude",
+                    color="lfm_instantflow",
+                    kind="scatter",
+                    facet_col="lap_lap",
+                    facet_col_wrap=4,
+                )
+
+            case "Flow-Dist":
+                print("Generating flow-dist...")
+                fig = df.plot(x="lap_dist", y="lfm_instantflow", color="lap_lap")
+
+            # this one has dosen't just use data straigh from the file, it caculates accel and conspuption for a unique graph
+            case "Acel-Speed-Dotplot":
+                print("Generating acel-speed-dotplot...")
+                df["acceleration"] = 0.0
+                df["consumption"] = 0.0
                 window_len = 30
 
                 for ind in range(window_len, len(df)):
                     ind_prev = ind - window_len
-                    df.loc[ind, 'acceleration'] = (df.loc[ind, 'gps_speed'] - df.loc[ind_prev, 'gps_speed']) / (df.loc[ind, 'obc_timestamp'] - df.loc[ind_prev, 'obc_timestamp'])
-                    df.loc[ind, 'consumption'] = (df.loc[ind, 'lfm_integratedcorrflow'] - df.loc[ind_prev, 'lfm_integratedcorrflow']) / (df.loc[ind, 'obc_timestamp'] - df.loc[ind_prev, 'obc_timestamp'])
+                    df.loc[ind, "acceleration"] = (
+                        df.loc[ind, "gps_speed"] - df.loc[ind_prev, "gps_speed"]
+                    ) / (
+                        df.loc[ind, "obc_timestamp"] - df.loc[ind_prev, "obc_timestamp"]
+                    )
+                    df.loc[ind, "consumption"] = (
+                        df.loc[ind, "lfm_integratedcorrflow"]
+                        - df.loc[ind_prev, "lfm_integratedcorrflow"]
+                    ) / (
+                        df.loc[ind, "obc_timestamp"] - df.loc[ind_prev, "obc_timestamp"]
+                    )
 
-                fig = df.plot.scatter(x='acceleration', y='gps_speed', color='consumption')
-            
-            case 'CorrFlow-Dist':
-                print('Generating CorrFlow-Dist')
-                fig = df.plot(x='lap_dist', y='lap_lfm_integratedcorrflow', color='lap_lap')
-            
-            case 'Joule-Dist':
-                print('Generating joule-dist...')
-                fig = df.plot(x='lap_dist', y='lap_jm3_netjoule', color='lap_lap')
-            
-            case 'BE-Joule-map':
-                print('Generating BE-Joule-map...')
-                fig = df.plot(x='gps_longitude', y='gps_latitude', color='jm3_current', kind='scatter', facet_col='lap_lap', facet_col_wrap=4)
-            
-            case 'Current-Dist':
-                print('Generating Current-dist...')
-                fig = df.plot(x='lap_dist', y='jm3_current', color='lap_lap')
-            
+                fig = df.plot.scatter(
+                    x="acceleration", y="gps_speed", color="consumption"
+                )
+
+            case "CorrFlow-Dist":
+                print("Generating CorrFlow-Dist")
+                fig = df.plot(
+                    x="lap_dist", y="lap_lfm_integratedcorrflow", color="lap_lap"
+                )
+
+            case "Joule-Dist":
+                print("Generating joule-dist...")
+                fig = df.plot(x="lap_dist", y="lap_jm3_netjoule", color="lap_lap")
+
+            case "Joule-map":
+                print("Generating Joule-map...")
+                fig = df.plot(x="gps_longitude",y="gps_latitude",color="jm3_current",kind="scatter",facet_col="lap_lap",facet_col_wrap=4,)
+
+            case "Current-Dist":
+                print("Generating Current-dist...")
+                fig = df.plot(x="lap_dist", y="jm3_current", color="lap_lap")
+
             case _:
                 print(Fore.YELLOW + f"Graph type '{graph_type}' is not recognized.")
                 return
 
         fig.show()
 
-    # Commands 
+    # Commands
     def do_quit(self, line):
         """Exit the CLI."""
         print(Fore.RED + "Are you sure you want to quit?")
@@ -169,78 +203,98 @@ class CLI(cmd.Cmd):
             print("Quit cancelled")
 
     def do_graph_select(self, line):
-        """Generate a specific graph from a list of options."""
+        """Generate a specific graph from a filtered list of options."""
         print("Generating Graph...")
 
         file_path, _ = self.select_data_file()
         if not file_path:
             return
-        
-        # Read the data
-        pd.options.plotting.backend = 'plotly'
-        pio.templates.default = 'plotly_dark'
+
+        # Read CSV
+        pd.options.plotting.backend = "plotly"
+        pio.templates.default = "plotly_dark"
+
         try:
-            df = pd.read_csv(file_path, sep=',', low_memory=False)
-            df = df.loc[df['lap_dist'] < 4000]
+            df = pd.read_csv(file_path, sep=",", low_memory=False)
+            df = df.loc[df["lap_dist"] < 4000]
         except Exception as e:
             print(Fore.YELLOW + f"Error reading CSV file: {e}")
             return
 
-        # Graph Descriptions
-        print(Fore.RED + "What graph type would you like to generate?")
-        print('')
-        print(Fore.RED + 'Engine Type Agnostic')
-        print("Map: a map of the course using GPS")
-        print("Speed-dist: Speed at different distances")
-        print(Fore.RED + 'ICE Specific:')
-        print("Map-Flow: A GPS map of the track overlayed with data from the flow meter")
-        print("Flow-Dist: Flow rate at different distances")
-        print("Acel-Speed-Dotplot: Dotplot of acceleration, speed, and fuel consumption")
-        print("CorrFlow-Dist: The lifetime fuel flow in relation to distance")
-        print(Fore.RED + "BE Specific:")
-        print("Joule-Dist: Net amount of energy used at different distances")
-        print("BE-Joule-map: A GPS map of the course overlayed by energy consumption")
-        print("Current-Dist: Current consumption at different points in the track")
+        # Select Engine Type
+        engine_question = [
+            inquirer.List(
+                "engine",
+                message=Fore.RED + "Select Engine Type",
+                choices=["ICE", "BE"],
+            )
+        ]
+    
+        engine_type = inquirer.prompt(engine_question)["engine"]
+    
+        # Filter graph list
+        if engine_type == "ICE":
+            graph_choices = self.ENGINE_AGNOSTIC_GRAPHS + self.ICE_GRAPHS
+        else:
+            graph_choices = self.ENGINE_AGNOSTIC_GRAPHS + self.BE_GRAPHS
 
-        # Uses inquirer to select from a list
+        # Select Graph
         questions = [
             inquirer.List(
                 "type",
                 message=Fore.RED + "Select a Graph",
-                choices=["Map", "Speed-Dist", "Map-Flow", "Flow-Dist", "Acel-Speed-Dotplot", 'CorrFlow-Dist', "Joule-Dist",  'BE-Joule-map', 'Current-Dist',]
+                choices=graph_choices,
             ),
         ]
-        graph_type = inquirer.prompt(questions)["type"]
-        print(graph_type)
-        self.generate_graph(df, graph_type)
 
+        graph_type = inquirer.prompt(questions)["type"]
+    
+        self.generate_graph(df, graph_type)
+        
     def do_graph(self, line):
         """Generate standard graphs from the telemetry data."""
         print("Generating Standard Graph Set.")
-        
+
         file_path, _ = self.select_data_file()
         if not file_path:
             return
-        
-        # Read the data
-        pd.options.plotting.backend = 'plotly'
-        pio.templates.default = 'plotly_dark'
+
+        pd.options.plotting.backend = "plotly"
+        pio.templates.default = "plotly_dark"
 
         try:
-            df = pd.read_csv(file_path, sep=',', low_memory=False)
-            df = df.loc[df['lap_dist'] < 4000]
+            df = pd.read_csv(file_path, sep=",", low_memory=False)
+            df = df.loc[df["lap_dist"] < 4000]
         except Exception as e:
             print(Fore.YELLOW + f"Error reading CSV file: {e}")
             return
 
-        # Generate multiple graphs
-        print("Generating graphs...")
-        self.generate_graph(df, 'Map')
-        self.generate_graph(df, 'Map-Flow')
-        self.generate_graph(df, 'Flow-Dist')
-        self.generate_graph(df, 'CorrFlow-Dist')
-        self.generate_graph(df, 'Speed-Dist')
-        self.generate_graph(df, 'Acel-Speed-Dotplot')
+        # Engine selection
+        engine_question = [
+            inquirer.List(
+                "engine",
+                message=Fore.RED + "Select Engine Type",
+                choices=["ICE", "BE"],
+            )
+        ]
 
-if __name__ == '__main__':
+        engine_type = inquirer.prompt(engine_question)["engine"]
+        print("Generating graphs...")
+
+    # Engine agnostic always runs
+        for graph in self.ENGINE_AGNOSTIC_GRAPHS:
+            self.generate_graph(df, graph)
+
+    # Engine specific
+        if engine_type == "ICE":
+            for graph in self.ICE_GRAPHS:
+                self.generate_graph(df, graph)
+        else:
+            for graph in self.BE_GRAPHS:
+                self.generate_graph(df, graph)
+
+# Actually calls and runs the program
+if __name__ == "__main__":
     CLI().cmdloop()
+
+
